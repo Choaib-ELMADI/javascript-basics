453922337cd9a0f350b0392e0a75b1e9790e0c7b
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-10-17 08:37:23 +0100

    Objects in JavaScript

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-
+const date = new Date();
+console.log(date.getDate()); // Day of the month
+console.log(date.getDay()); // Day of the week
+console.log(date.getTime()); // Time in milliseconds since 1970
